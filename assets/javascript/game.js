d8f7d0e2f945e0fdbd02f4cc477324be126c039e
eggobiggie <miacrandell@gmail.com>
2017-11-15 20:21:09 -0700

    add javascript and images for winning hangman

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,9 @@ var wordChoice = wordArray[Math.floor(Math.random() * wordArray.length)];
 console.log(wordChoice);
 
 var alreadyGuessed = [];
+
+//THINGS NEEDED: Verification of alphabet and TOLOWERCASE.
+
 guessesRemainingFunction();
 
   document.onkeypress = function(event) {
@@ -27,6 +30,23 @@ function wordCheck(userGuess) {
       if (userGuess === wordChoice[i]) {
         doesItMatch = true;
         playSpace[i] = userGuess;
+      }
+    }
+    var didWeWin = true;
+      for (i = 0; i < wordChoice.length; i++) {
+        if (playSpace[i] != wordChoice[i]) {
+          didWeWin = false;
+        }
+      }
+      if (didWeWin === true) {
+        winnerFunction();
+        //return;
+      }
+
+
+    for (i = 0; i < alreadyGuessed.length; i++) {
+      if (userGuess === alreadyGuessed[i]) {
+        return;
       }
     }
     if (!doesItMatch) {
@@ -66,10 +86,58 @@ function drawPlaySpace() {
 
  drawPlaySpace();
 
+ function winnerFunction() {
+   //need to add style so that the margin is changed for each, need to add each image and make them the right spot
+   //need to make each image visible as well.
+   //ADD IMAGES TO HTML
+   //also need to make it so that the win-count goes up
+   switch (wordChoice) {
+     case "linda":
+     document.getElementById("bobFamily").style.visibility = "hidden";
+     document.getElementById("linda").style.visibility = "visible";
+     document.getElementById("resultTextChange").innerHTML = "Alriiiiight! It's Linda!";
+     break;
+     case "beefsquatch":
+     document.getElementById("bobFamily").style.visibility = "hidden";
+     document.getElementById("beefsquatch").style.visibility = "visible";
+     document.getElementById("resultTextChange").innerHTML = "THIS IS ME NOW! It's Beefsquatch!";
+     break;
+     case "fischoeder":
+     document.getElementById("bobFamily").style.visibility = "hidden";
+     document.getElementById("fischoeder").style.visibility = "visible";
+     document.getElementById("resultTextChange").innerHTML = "You can work in my coal mine! It's Calvin Fischoeder!";
+     break;
+     case "louise":
+     document.getElementById("bobFamily").style.visibility = "hidden";
+     document.getElementById("louise").style.visibility = "visible";
+     document.getElementById("resultTextChange").innerHTML = "Enough with Canada! It's Louise!";
+     break;
+     case "marshmallow":
+     document.getElementById("bobFamily").style.visibility = "hidden";
+     document.getElementById("marshmallow").style.visibility = "visible";
+     document.getElementById("resultTextChange").innerHTML = "If you show me a sweet potato pie, I am on top of it. It's Marshmallow!";
+     break;
+     case "frond":
+     document.getElementById("bobFamily").style.visibility = "hidden";
+     document.getElementById("frond").style.visibility = "visible";
+     document.getElementById("resultTextChange").innerHTML = "Don't throw Repressed Memory Emily! It's Mr. Frond!";
+     break;
+     case "mudflap":
+     document.getElementById("bobFamily").style.visibility = "hidden";
+     document.getElementById("mudflap").style.visibility = "visible";
+     document.getElementById("resultTextChange").innerHTML = "Oh, Mudflap? Uh that was my grandmother's name. It's Mudflap!";
+     break;
+   }
+ }
+
  function loserFunction() {
    if (guessesRemaining === 0) {
     document.getElementById("bobFamily").style.visibility = "hidden";
     document.getElementById("tinaSad").style.visibility = "visible";
     document.getElementById("resultTextChange").innerHTML = "You Lost! Try again!";
+    guessesRemaining = 12;
+    wordChoice = wordArray[Math.floor(Math.random() * wordArray.length)];
+    console.log(wordChoice);
+    playSpace = drawBlanks();
     };
  }
